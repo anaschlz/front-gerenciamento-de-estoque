c5d4b287ea0f04b3d06649abd85253de0772e502
anaschlz <analu.sschulz@gmail.com>
2024-05-10 03:00:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,142 @@
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Button,
+  useDisclosure,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ModalComp from './components/ModalComp';
+
+interface Product {
+  product_id: number;
+  product_name: string;
+  product_amount: number;
+  product_price: number;
+}
+
+const App = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [products, setProducts] = useState<Product[]>([]); 
+  const [productToEdit, setProductToEdit] = useState<Product | null>(null);
+
+
+  const isMobile = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("https://api-gerenciamento-de-estoque.vercel.app/api/");
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const handleRemove = async (productId: number) => {
+    try {
+      await axios.delete(`https://api-gerenciamento-de-estoque.vercel.app/api/data/${productId}/`);
+      setProducts(products.filter(product => product.product_id !== productId));
+    } catch (error) {
+      console.error("Error deleting product:", error);
+    }
+  };
+
+  const handleEdit = (product: Product) => {
+    setProductToEdit(product);
+    onOpen();
+  };
+
+  const handleOpenModal = () => {
+    setProductToEdit(null);
+    onOpen();
+  };
+
+
+  const productToEditOrDefault = productToEdit || { product_id: 0, product_name: "", product_amount: 0, product_price: 0 };
+
+return (
+  <Flex
+    h="100vh"
+    align="center"
+    justify="center"
+    fontSize="20px"
+    fontFamily="poppins"
+  >
+    <Box maxW={800} w="100%" h="100vh" py={10} px={2}>
+      <Button colorScheme="blue" onClick={handleOpenModal}>
+        NOVO PRODUTO
+      </Button>
+
+      <Box overflowY="auto" height="100%">
+        <Table mt="7">
+          <Thead>
+            <Tr>
+            <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                ID
+              </Th>
+              <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                Nome
+              </Th>
+              <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                Quantidade
+              </Th>
+              <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                Preço
+              </Th>
+              <Th p={0}></Th>
+              <Th p={0}></Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {products.map(({ product_id, product_name, product_amount, product_price }) => (
+              <Tr key={product_id} cursor="pointer" _hover={{ bg: "gray.100" }}>
+                <Td maxW={isMobile ? 5 : 100}>{product_id}</Td>
+                <Td maxW={isMobile ? 5 : 100}>{product_name}</Td>
+                <Td maxW={isMobile ? 5 : 100}>{product_amount}</Td>
+                <Td maxW={isMobile ? 5 : 100}>{product_price}</Td>
+                <Td p={0}>
+                  <EditIcon
+                    fontSize={20}
+                    onClick={() => handleEdit({ product_id, product_name, product_amount, product_price })}
+                  />
+                </Td>
+                <Td p={0}>
+                  <DeleteIcon
+                    fontSize={20}
+                    onClick={() => handleRemove(product_id)} 
+                  />
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+    {isOpen && (
+      <ModalComp
+        setProductToEdit={setProductToEdit} 
+        isOpen={isOpen}
+        onClose={onClose}
+        product={productToEditOrDefault}
+        setProducts={setProducts}
+      />
+    )}
+  </Flex>
+);
+}
+export default App;
---
+++ src/components/ModalComp.tsx
@@ -0,0 +1,132 @@
+import { useState } from "react";
+import axios from "axios";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+} from "@chakra-ui/react";
+
+interface Product {
+  product_id: number;
+  product_name: string;
+  product_amount: number;
+  product_price: number;
+}
+
+interface Props {
+  product: any;
+  setProducts: any;
+  isOpen: any;
+  onClose: any;
+  setProductToEdit: (product: Product | null) => void;
+}
+
+const ModalComp = ({ product, setProducts, isOpen, onClose}: Props) => {
+  const [productName, setProductName] = useState(product.product_name || "");
+  const [productAmount, setProductAmount] = useState(Number(product.product_amount) || 0);
+  const [productPrice, setProductPrice] = useState(Number(product.product_price) || 0);
+
+  const handleSave = async () => {
+    if (!productName || !productAmount || !productPrice) return;
+
+    try {
+      const data = {
+        product_name: productName,
+        product_amount: productAmount,
+        product_price: productPrice,
+      };
+
+      if (product.product_id) {
+        await axios.put(`https://api-gerenciamento-de-estoque.vercel.app/api/product/${product.product_id}/`, data);
+      } else {
+        await axios.post(`https://api-gerenciamento-de-estoque.vercel.app/api/data/${product.product_id}/`, data);
+      }
+
+      const response = await axios.get("https://api-gerenciamento-de-estoque.vercel.app/api/");
+      setProducts(response.data);
+
+      onClose();
+    } catch (error) {
+      console.error("Error saving product:", error);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`https://api-gerenciamento-de-estoque.vercel.app/api/data/${product.product_id}/`);
+
+      const response = await axios.get("https://api-gerenciamento-de-estoque.vercel.app/api/");
+      setProducts(response.data);
+
+      onClose();
+    } catch (error) {
+      console.error("Error deleting product:", error);
+    }
+  };
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{product.product_id ? "Editar Produto" : "Novo Produto"}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl display="flex" flexDir="column" gap={4}>
+              <Box>
+                <FormLabel>Nome do Produto</FormLabel>
+                <Input
+                  type="text"
+                  value={productName}
+                  onChange={(e) => setProductName(e.target.value)}
+                />
+              </Box>
+              <Box>
+                <FormLabel>Quantidade</FormLabel>
+                <Input
+                  type="number"
+                  value={productAmount}
+                  onChange={(e) => setProductAmount(Number(e.target.value))}
+                />
+              </Box>
+              <Box>
+                <FormLabel>Preço</FormLabel>
+                <Input
+                  type="text"
+                  pattern="[0-9]*[.,]?[0-9]*"
+                  value={productPrice}
+                  onChange={(e) => setProductPrice(Number(e.target.value))}
+                />
+              </Box>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter justifyContent="start">
+            <Button colorScheme="green" mr={3} onClick={handleSave}>
+              {product.product_id ? "Atualizar" : "Salvar"}
+            </Button>
+            {product.product_id && (
+              <Button colorScheme="red" onClick={handleDelete}>
+                Excluir
+              </Button>
+            )}
+            <Button colorScheme="gray" ml={2} onClick={onClose}>
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalComp;
